d52d1d63ededc260bd4ec4dd1e260e6163283e81
scratcheradvik-dotcom <scratcheradvik@gmail.com>
2025-10-16 19:23:28 -0700

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,32 @@
+music.play(music.stringPlayable("- - - - - - - - ", 120), music.PlaybackMode.UntilDone)
+basic.showLeds(`
+    . . . . .
+    . # # . .
+    . # # . .
+    . . . . .
+    . . . . .
+    `)
+basic.showLeds(`
+    # . . . .
+    . # . . .
+    . . # . .
+    . . . . .
+    . . . . .
+    `)
+basic.showLeds(`
+    . . . . .
+    . # # # .
+    . # . # .
+    . # # # #
+    . . . . .
+    `)
+basic.showLeds(`
+    . . . . .
+    . . . . .
+    . . . # .
+    # # # # .
+    . . . . .
+    `)
+basic.forever(function () {
+	
+})
